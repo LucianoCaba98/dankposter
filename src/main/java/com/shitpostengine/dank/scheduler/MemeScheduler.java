3b54f10f8448db66133dc8a32c30382af7839740
R2CABA <lucianocabanillas98@gmail.com>
2025-07-09 20:51:59 -0300

    - fixed logic for pÃosting and fetching memes

--- src/main/java/com/shitpostengine/dank/scheduler/MemeScheduler.java
+++ src/main/java/com/shitpostengine/dank/scheduler/MemeScheduler.java
@@ -1,24 +1,24 @@
 package com.shitpostengine.dank.scheduler;
 
 import com.shitpostengine.dank.service.DiscordPosterService;
-import com.shitpostengine.dank.service.RedditMemeService;
+import com.shitpostengine.dank.service.RedditFetcherService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemeScheduler {
 
-    private final RedditMemeService redditMemeService;
+    private final RedditFetcherService redditFetcherService;
     private final DiscordPosterService discordPosterService;
 
-    public MemeScheduler(RedditMemeService redditMemeService, DiscordPosterService discordPosterService) {
-        this.redditMemeService = redditMemeService;
+    public MemeScheduler(RedditFetcherService redditFetcherService, DiscordPosterService discordPosterService) {
+        this.redditFetcherService = redditFetcherService;
         this.discordPosterService = discordPosterService;
     }
 
     @Scheduled(fixedRateString = "${scheduling.fetch-interval-ms}")
     public void memeFetcher() {
-        redditMemeService.fetchMemesFromSubreddit();
+        redditFetcherService.fetchMemesFromSubreddit();
     }
 
     @Scheduled(fixedRateString = "${scheduling.post-interval-ms}")
